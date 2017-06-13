e7d14f9a9d42ebf0a0787e3c8648bc49e5d950dd
darkwoolf10 <woolftreuser@yandex.com>
2017-06-13 17:04:16 +0300

    first pull request by lonely

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=TABLES',
     'username' => 'root',
-    'password' => '',
+    'password' => '12superman',
     'charset' => 'utf8',
+    'tablePrefix' => 'tbl_'
 ];
